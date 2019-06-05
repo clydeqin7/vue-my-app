1300c84411264dcf882431c33662c6e9aa1ebe5e
Aqin7 <clydeqin7@foxmail.com>
2019-06-05 10:56:19 +0800

    项目依赖安装；06 | Vue组件的核心概念(1):属性

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,15 @@
 <template>
   <div id="app">
-      {{msg}}
-      <div>
-        <input type="text" v-model="info"/>
-        <button v-on:click="handleClick">添加</button>
-      </div>
-      <ul>
-        <todo-item v-for="(item, index) in list" :key="index">
-          <template v-slot:slotXXX="solotXXXProps">
-            <span :style="{fontSize: '20px', color: solotXXXProps.checked ? 'red' : 'blue' }">{{item}}</span>
-          </template>
-        </todo-item>
-      </ul>
+      <router-view />
   </div>
 </template>
 
-<script>
-import TodoItem from './components/TodoItem.vue'
-
-export default {
-  name: 'app',
-  components: {
-    TodoItem
-  },
-  data() {
-    return {
-      msg: 'hello world',
-      info: '',
-      list: []
-    }
-  },
-  methods: {
-    handleClick() {
-      this.list.push(this.info)
-      this.info = ''
-    }
-  }
+<style lang="less">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
-
-<style>
-
 </style>
---
+++ src/views/1.1/index.vue
@@ -0,0 +1,40 @@
+<!--  -->
+<template>
+  <div>
+    <a-tabs>
+      <a-tab-pane key="props" tab="属性">
+        <Props
+          name="Hello Vue!"
+          :type="type"
+          :is-visible="false"
+          :on-change="handlePropChange"
+          title="属性Demo"
+          class="test1"
+          :class="['test2']"
+          :style="{ marginTop: '20px' }"
+          stype="margin-top: 10px"
+        ></Props>
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script>
+import Props from './Props.vue'
+export default {
+  components: { Props },
+  data () {
+    return {
+      type: 'danger'
+    }
+  },
+  methods: {
+    handlePropChange(val) {
+      this.type = val
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
