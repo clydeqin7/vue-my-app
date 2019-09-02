bc0b6058698f8b0daf76ccd21686332bbb67163c
clydeqin7 <clydeqin7@foxmail.com>
2019-09-02 12:40:54 +0800

    1.5 计算属性 vs 侦听器

---
+++ 视频DEMO源码/src/views/1.5/WatchMy.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    {{ fullName }}
+    <div>
+      firstName:
+      <input v-model="firstName" />
+    </div>
+    <div>
+      lastName:
+      <input v-model="lastName" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      firstName: "Foo",
+      lastName: "Bar",
+      fullName: "Foo Bar"
+    };
+  },
+  watch: {
+    firstName: function(val) {
+      window.clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        this.fullName = val + " " + this.lastName;
+      }, 1000);
+    },
+    lastName: function(val) {
+      window.clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        this.fullName = this.firstName + " " + val;
+      }, 1000);
+    }
+  }
+};
+</script>
